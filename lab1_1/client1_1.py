2f2d25359839a8babf71457dbcab7539b84a145d
Serhiy-Mysyshyn <serhiymysyshyn@gmail.com>
2021-10-08 00:06:44 +0300

    Lab 1 (Ex 1.1)

---
+++ lab1_1/client1_1.py
@@ -0,0 +1,20 @@
+import socket
+import sys
+
+tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)          # Задаю сім'ю протоколів і тип передачі даних
+tcp_socket.connect(("localhost", 777))                                  # Прив'язую сокет до мережевого адаптера
+
+while True:
+    data = input('  [ Client -> Server ] : ')                           # Поле для вводу інформації з боку клієнта
+    if not data or data=="/close_connection":                           # Перевіряю дані: чи не пусте значення, чи не спец.фраза для закриття з'єднання
+        tcp_socket.close()                                              # У разі виконання -> закриваю сокет і для клієнта
+        print("\n[ ! ] Connection closed")
+        sys.exit(1)
+    data = str.encode(data)                                             # encode - кодування в байти, decode - розкодування з байтів
+    tcp_socket.send(data)                                               # Надсилаю дані на сервер
+    data = bytes.decode(data)
+    data = tcp_socket.recv(1024)                                        # Отримую опрацьовані дані із сервера
+    print(data)
+    break
+
+tcp_socket.close()
---
+++ lab1_1/server1_1.py
@@ -0,0 +1,27 @@
+import socket
+import time
+from time import strftime, gmtime
+
+tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)          # Задаю сім'ю протоколів і тип передачі даних
+tcp_socket.bind(("localhost", 777))                                     # Прив'язую сокет до мережевого адаптера
+tcp_socket.listen(1)                                                    # Задаю розмір черги вхідних підключень
+
+while True:
+    connection, address = tcp_socket.accept()  # Отримую з'єднання із черги вхідних підключень
+    data = connection.recv(1024)                                        # Отримую передане значення від клієнта
+    newData = bytes.decode(data)                                        # Розкодовую з байтів
+    dataTime = strftime("%Y-%m-%d %H:%M:%S", gmtime())                  # Отримую значення дати і часу
+    if not data:                                                       # Перевіряю дані: чи не пусте значення, чи не спец.фраза для закриття з'єднання
+        connection.close()                                              # У разі виконання: закриваю з'єднання
+        print("[ ! ] Connection closed")
+        break
+    else:
+        print("[ Client -> Server ] : " + newData)                      # Виводжу надіслані дані від користувача (для зручності)
+        newData = "[ Server -> Client ] : ("+dataTime+") "+newData      # Формую дані, які буду надсилати назад користувачу
+        print(newData)                                                  # Виводжу дані, які будуть надіслані назад користувачу (для зручності)
+        data = str.encode(newData)                                      # Кодую дані для відправки в байти
+        connection.send(data)                                           # Надсилаю дані назад користувачу
+        connection.close()                                              # Закриваю сокет
+        break
+
+tcp_socket.close()
