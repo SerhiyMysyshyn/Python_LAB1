7e18d7470d1d3e32cb0518caaf1c4d6e1b5250ac
Serhiy-Mysyshyn <serhiymysyshyn@gmail.com>
2021-10-08 01:49:29 +0300

    Lab 1 (Ex 1.3 and Ex 1.4)

--- lab1_1/client1_1.py
+++ lab1_1/client1_1.py
@@ -6,23 +6,15 @@ tcp_socket.connect(("localhost", 777))                                  # При
 
 while True:
     data = input('  [ Client -> Server ] : ')                           # Поле для вводу інформації з боку клієнта
-    if not data:                                                        # Перевіряю дані: чи не пусте значення
+    if not data or data=="/close_connection":                           # Перевіряю дані: чи не пусте значення, чи не спец.фраза для закриття з'єднання
         tcp_socket.close()                                              # У разі виконання -> закриваю сокет і для клієнта
         print("\n[ ! ] Connection closed")
         sys.exit(1)
-    dataSize = len(data)
-    dataSize = dataSize.__str__()
     data = str.encode(data)                                             # encode - кодування в байти, decode - розкодування з байтів
-    dataSize = str.encode(dataSize)
-
     tcp_socket.send(data)                                               # Надсилаю дані на сервер
-    tcp_socket.send(dataSize)                                              # Надсилаю дані на сервер (розмір даних)
-
     data = bytes.decode(data)
     data = tcp_socket.recv(1024)                                        # Отримую опрацьовані дані із сервера
     print(data)                                                         # Виводжу опрацьовані дані клієнту
-    break
+    print()
 
-tcp_socket.close()
-
-
+# /close_connection - команда для закриття з'єднання
--- lab1_1/server1_1.py
+++ lab1_1/server1_1.py
@@ -6,27 +6,26 @@ tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)          # Зад
 tcp_socket.bind(("localhost", 777))                                     # Прив'язую сокет до мережевого адаптера
 tcp_socket.listen(1)                                                    # Задаю розмір черги вхідних підключень
 
+print('[ ! ] Підключення...')
+connection, address = tcp_socket.accept()                               # Отримую з'єднання із черги вхідних підключень
+print('[ ! ] Підключено')
+print('[ ! ] Адреса клієнта: ', address)                                # Виводжу адресу клієнта (для зручності)
+print('__________________________________________________________________')
 while True:
-    connection, address = tcp_socket.accept()
     data = connection.recv(1024)                                        # Отримую передане значення від клієнта
-    dataSize = connection.recv(1024)                                    # Отримую передане значення від клієнта
     newData = bytes.decode(data)                                        # Розкодовую з байтів
-    dataSize = bytes.decode(dataSize)
     dataTime = strftime("%Y-%m-%d %H:%M:%S", gmtime())                  # Отримую значення дати і часу
-    if not data:                                                        # Перевіряю дані: чи не пусте значення
+    if not data or newData == "/close_connection":                      # Перевіряю дані: чи не пусте значення, чи не спец.фраза для закриття з'єднання
         connection.close()                                              # У разі виконання: закриваю з'єднання
         print("[ ! ] Connection closed")
         break
-    elif (len(data) != int(dataSize)):
-        print("[ Server ] Error! Lost data!")
     else:
-        print("[ Server ] Recieved data equals Send data!")
         print("[ Client -> Server ] : " + newData)                      # Виводжу надіслані дані від користувача (для зручності)
         newData = "[ Server -> Client ] : ("+dataTime+") "+newData      # Формую дані, які буду надсилати назад користувачу
-        time.sleep(5)                                                   # Задаю затримку відповіді сервера
-        print(newData)  # Виводжу дані, які будуть надіслані назад користувачу (для зручності)
-        data = str.encode(newData)  # Кодую дані для відправки в байти
-        connection.send(data)  # Надсилаю дані назад користувачу
-        break
+        #time.sleep(5)                                                   # Задаю затримку відповіді сервера
+        print(newData)                                                  # Виводжу дані, які будуть надіслані назад користувачу (для зручності)
+        data = str.encode(newData)                                      # Кодую дані для відправки в байти
+        connection.send(data)                                           # Надсилаю дані назад користувачу
+        print("")
 
 tcp_socket.close()                                                      # Закриваю сокет
